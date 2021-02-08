81ae413ecb2c48537c8d0bfc7e37979d19a24b8f
cpy <398129519@qq.com>
2021-02-08 18:36:05 +0800

    Spring boot2 Test 修改UserController

--- 后端/web01/src/main/java/com/web/web01/controller/UseController.java
+++ 后端/web01/src/main/java/com/web/web01/controller/UseController.java
@@ -8,26 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestController
 public class UseController {
 
-//    @RequestMapping(value = "/user",method = RequestMethod.GET)
-//    public String getUser(){
-//        return "User Get";
-//    }
-//    @GetMapping("/user") // = @RequestMapping(value = "/User", method = RequestMethod.POST)
-//    public String postUser(){
-//        return "User Post";
-//    }
-//
-//    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
-//    public String deleteUser(){
-//        return "User Delete";
-//    }
-//
-//    @RequestMapping(value = "/user",method = RequestMethod.PUT)
-//    public String putUser(){
-//        return "User Put";
-//    }
-//
-////    用postman等请求是不用开启Rest功能的
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "User Get";
+    }
+    @GetMapping("/user") // = @RequestMapping(value = "/User", method = RequestMethod.POST)
+    public String postUser(){
+        return "User Post";
+    }
 
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "User Delete";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "User Put";
+    }
+
+//    用postman等请求是不用开启Rest功能的
+//    有一次试过这个conctroller无法运行
 
 }
