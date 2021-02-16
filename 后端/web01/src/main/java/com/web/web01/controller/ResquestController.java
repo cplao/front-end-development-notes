039c5d47a7b76c1cc8d14ea671acc01428fea439
cpy <398129519@qq.com>
2021-02-16 12:49:42 +0800

    setAttribute的用法

---
+++ 后端/web01/src/main/java/com/web/web01/controller/ResquestController.java
@@ -0,0 +1,39 @@
+package com.web.web01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ResquestController {
+
+    @GetMapping("/goto")
+    public String goToPage(HttpServletRequest resquest){
+
+        resquest.setAttribute("msg","成功了");
+        resquest.setAttribute("code","200");
+        return "forward:/success"; //转发到/success请求
+
+    }
+
+    @ResponseBody
+    @GetMapping("/success")
+    public Map success(@RequestAttribute("msg") String msg,
+                       @RequestAttribute("code") Integer code,
+                       HttpServletRequest resquest){
+
+        Object msg1 = resquest.getAttribute("msg");
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("reqMethod_msg",msg1);
+        map.put("annotation_msg",msg);
+
+        return map;
+    }
+}
